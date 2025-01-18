5770c5d4acb5ea82ba3ae0311fd80a46bc670a71
binayakbhandari <binayakbhandari60@gmail.com>
2025-01-18 23:57:16 +0545

    Configured CORS

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,24 @@
 import express from 'express'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import noteRoute from './note/noteRoute'
+import envConfig from './config/config'
+import cors from 'cors'
+
 const app = express()
+
+// PARSE INCOMING JSON TO HANDLE UNDEFINED ERROR
 app.use(express.json())
 
+// CORS CONFIGURATION
+app.use(cors({
+    origin : envConfig.frontendUrl
+}))
+
 app.use("/api/notes",noteRoute)
+
+// IMAGE PUBLIC
 app.use(express.static("./src/uploads/"))
+
+// ERROR HANDLER
 app.use(globalErrorHandler)
 export default app
